c3c01fec2276cf88895ab800d6e74ee751ab2e36
boomworks <boomworks@d4f3cd64-9e9b-11dd-9929-29c1920dd198>
2008-10-31 16:07:24 +0000

    add release folder

---
+++ SrcCounter/release/release_script.bat
@@ -0,0 +1,21 @@
+REM Start
+w:
+cd w:\boomworks\SrcCounter
+
+copy /Y src\bin\release\*.exe release\sourcecounter\
+xcopy /Y /f /s src\img\*.* release\sourcecounter\img\
+xcopy /Y /f /s src\locales\*.mo release\sourcecounter\locales\
+
+cd release\
+copy /Y *.dll sourcecounter\
+copy /Y *.txt sourcecounter\
+
+REM delete 7z zip files
+del *.7z *.zip
+
+REM 7z a -t7z Files.7z *.txt -r
+7z a -t7z sourcecounter-2.0.9.54.7z SourceCounter\
+REM zip
+7z a -tzip sourcecounter-2.0.9.54.zip SourceCounter\ -mx9
+
+REM End
