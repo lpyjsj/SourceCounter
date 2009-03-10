e9c00868359f259096e7f1a6a2edca01ddd6a56d
boomworks <boomworks@d4f3cd64-9e9b-11dd-9929-29c1920dd198>
2009-03-10 05:23:33 +0000

    UP

--- SrcCounter/release/release_script.bat
+++ SrcCounter/release/release_script.bat
@@ -25,8 +25,8 @@ delete 7z zip files
 del *.7z *.zip
 
 REM 7z a -t7z Files.7z *.txt -r
-7z a -t7z SourceCounter-2.2.30.36.7z SourceCounter\ -mx7
+7z a -t7z SourceCounter-2.2.32.49.7z SourceCounter\ -mx7
 REM zip
-7z a -tzip SourceCounter-2.2.30.36.zip SourceCounter\ -mx9
+7z a -tzip SourceCounter-2.2.32.49.zip SourceCounter\ -mx9
 
 REM End
