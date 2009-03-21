511d6bfd08f8f4d8843f39b31673cf08a9b107b0
boomworks <boomworks@d4f3cd64-9e9b-11dd-9929-29c1920dd198>
2009-03-21 14:55:56 +0000

    up release script.

--- SrcCounter/release/release_script-nozip.bat
+++ SrcCounter/release/release_script-nozip.bat
@@ -6,6 +6,8 @@ REM mkdir
 mkdir release\sourcecounter
 
 copy /Y src\bin\release\*.exe release\sourcecounter\
+copy /Y src\*.xml release\sourcecounter\
+
 xcopy /Y /f /s src\img\*.* release\sourcecounter\img\
 xcopy /Y /f /s src\locales\*.mo release\sourcecounter\locales\
 xcopy /Y /f /s src\locales\*.po release\sourcecounter\locales\
